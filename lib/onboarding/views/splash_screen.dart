740124bad28c9c7e095a573040d192f8d5d661be
Abdulrasheed Fawole <fawomath@gmail.com>
2022-07-12 18:52:15 +0100

    chore(implement design): added the file for splash screen

--- lib/onboarding/views/splash_screen.dart
+++ lib/onboarding/views/splash_screen.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:hack_with_io/home/views/home_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  /// Don't bother about this just continue with your work
+  ///
+  /// :)
+  Future<void> trasitToOnboardingPage() async {
+    final prefs = await SharedPreferences.getInstance();
+    final showOnbarding = prefs.getBool('showOnboarding') ?? false;
+    Future.delayed(
+      const Duration(seconds: 5),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return showOnbarding
+                ? const HomeView()
+                : const Center(
+                    child: Text('hello'),
+                  );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
